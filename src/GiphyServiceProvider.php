a3948ee02ccf2341fde3121b7b3d4911e0017944
Abdullah Ghanem <3bdullah.ghanem@gmail.com>
2021-07-23 01:41:24 +0200

    Update GiphyServiceProvider.php

--- src/GiphyServiceProvider.php
+++ src/GiphyServiceProvider.php
@@ -6,6 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class GiphyServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/giphy.php', 'giphy');
@@ -13,5 +18,17 @@ class GiphyServiceProvider extends ServiceProvider
         $this->app->bind('ghanem-giphy', function () {
             return new Giphy;
         });
+    }
+    
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/giphy.php' => config_path('giphy.php'),
+        ], 'config');
     }
 }
